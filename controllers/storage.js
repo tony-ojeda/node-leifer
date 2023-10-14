4677ca669585b3ab95e92a537d2a92cae7ebdb59
tony-ojeda <tojeda96@gmail.com>
2023-10-13 21:50:19 -0500

    feat: add validation in tracks

---
+++ controllers/storage.js
@@ -0,0 +1,53 @@
+const { storageModel } = require('../models')
+const PUBLIC_URL = process.env.PUBLIC_URL
+
+/**
+ * Obtener lista de la base de datos!
+ * @param {*} req 
+ * @param {*} res 
+ */
+const getItems = async (req, res) => {
+  const data = await storageModel.find({})
+
+  res.send({ data })
+}
+
+/**
+ * Obtener un detalle
+ * @param {*} req 
+ * @param {*} res 
+ */
+const getItem = (req, res) => {}
+
+/**
+ * Insertar un registro
+ * @param {*} req 
+ * @param {*} res 
+ */
+const createItem = async (req, res) => {
+  const { body, file } = req
+  const fileData = {
+    filename: file.filename,
+    url: `${PUBLIC_URL}/${file.filename}`
+  }
+  const data = await storageModel.create(fileData)
+  
+  res.send({ data })
+}
+
+/**
+ * Actualizar un registo
+ * @param {*} req 
+ * @param {*} res 
+ */
+const updateItem = (req, res) => {}
+
+/**
+ * Eliminar un registro
+ * @param {*} req 
+ * @param {*} res 
+ */
+const deleteItem = (req, res) => {}
+
+
+module.exports = { getItems, getItem, createItem, updateItem, deleteItem }
--- routes/tracks.js
+++ routes/tracks.js
@@ -1,9 +1,10 @@
 const express = require("express")
 const router = express.Router()
 
-router.get("/", ((req, res) => {
-  const data = ["hola", "mundo"]
-  res.send({data})
-}))
+const { validatorCreateItem } = require('../validators/tracks')
+const { getItems, getItem, createItem } = require("../controllers/tracks")
+
+router.get("/", getItems)
+router.post("/", validatorCreateItem, createItem)
 
 module.exports = router
